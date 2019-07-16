0a0730b2cc6ef1fe6c2259f4c9f7cb16360b0167
Attila Torok <torcsiattila93@gmail.com>
2019-07-16 15:55:09 +0200

    Domino Comparable

---
+++ week-04/day-2/src/Comparable/Domino.java
@@ -0,0 +1,33 @@
+package Comparable;
+
+    public class Domino implements Comparable<Domino> {
+
+        private final int left;
+        private final int right;
+
+    public Domino(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getLeftSide() {
+        return left;
+    }
+
+    public int getRightSide() {
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + left + ", " + right + "]";
+    }
+
+        @Override
+        public int compareTo(Domino o) {
+            if (this.left - o.left == 0) {
+                return this.right - o.right;
+            }
+            return this.left - o.left;
+        }
+    }
