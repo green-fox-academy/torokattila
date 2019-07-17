cdd459c899078fcc67eaadfcd1e806063fa4f815
Attila Torok <torcsiattila93@gmail.com>
2019-07-17 11:53:58 +0200

    SumTest

--- week-04/day-3/src/Sum/Sum.java
+++ week-04/day-3/src/Sum/Sum.java
@@ -1,4 +1,19 @@
 package Sum;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sum {
+
+    public int sum(List<Integer> list) {
+        int sum = 0;
+        if(list == null) {
+            return 0;
+        }
+        for (int i : list) {
+            sum += i;
+        }
+        return sum;
+    }
+
 }
--- week-04/day-3/src/Sum/SumTest.java
+++ week-04/day-3/src/Sum/SumTest.java
@@ -1,5 +1,46 @@
+package Sum;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SumTest {
 
+    Sum sum = new Sum();
+
+    @Test
+    void testSum() {
+        List<Integer> input = new ArrayList<>();
+        input.add(2);
+        input.add(3);
+        assertEquals(5, sum.sum(input));
+    }
+
+    @Test
+    void sumWorksWithEmptyList() {
+        List<Integer> input = new ArrayList<>();
+        assertEquals(0, sum.sum(input));
+    }
+
+    @Test
+    void sumWorksWithOneElement() {
+        List<Integer> input = new ArrayList<>();
+        input.add(1);
+        assertEquals(1, sum.sum(input));
+    }
+
+    @Test
+    void sumWorksWithMultipleElements() {
+        List<Integer> input = new ArrayList<>();
+        input.add(2);
+        assertEquals(2, sum.sum(input));
+    }
+
+    @Test
+    void sumWorksWithNull() {
+        assertEquals(0, sum.sum(null));
+    }
 }
