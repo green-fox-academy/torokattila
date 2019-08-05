b7c90375fcf6be62b9a06ac282152248d6cf93ef
Attila Torok <torcsiattila93@gmail.com>
2019-08-05 12:04:50 +0200

    Exercise2 is done

--- week-6/day-1/src/Exercise2.java
+++ week-6/day-1/src/Exercise2.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
 import java.util.List;
 
-public class Exercise1 {
+public class Exercise2 {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
         numbers.parallelStream()
-                .filter(number -> number % 2 == 0)
+                .filter(number -> number > 0)
+                .map(number -> number * number)
                 .forEach(System.out::println);
     }
 }
